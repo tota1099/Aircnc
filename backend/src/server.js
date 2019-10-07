5f2edf9ad5ca7e2124b728e25f97d361cab69faa
Renan Porto <renan.porto1099@gmail.com>
2019-10-06 23:29:19 -0300

    Adicionando socket.io para comunicar a requisiçao de um spot e aprovaçao/rejeiçao em tempo real

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,36 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 const config = require('config');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(config.DBHost, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+/* Em produção é recomendado usar o redis */
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+// middleware
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -21,5 +43,5 @@ app.use(routes);
 if(process.env.NODE_ENV === 'test'){
     module.exports = app;
 } else {
-    app.listen(3333);
+    server.listen(3333);
 }
